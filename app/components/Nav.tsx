4ca5734ed114afc632a92c0135c3d7e72f861207
austin1052 <austin1052@gmail.com>
2023-09-10 16:02:18 -0400

    updated nav menu style

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,32 +1,24 @@
 "use client";
 
 import styles from "@/app/styles/Nav.module.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export default function Nav() {
   const [navOpen, setNavOpen] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
-  // const [circleStyle, setCircleStyle] = useState();
-  const [navHidden, setNavHidden] = useState(true);
+  const [navHidden, setNavHidden] = useState(false);
 
-  const [navMenuStyle, setNavMenuStyle] = useState(
-    `${styles.navMenu} ${styles.navMenuHidden}`
-  );
-
-  const controlNavbar = () => {
+  const controlNavbar = useCallback(() => {
     if (typeof window !== "undefined") {
+      // need to check if windowScrollY is > 0 for phones that may have a bounce animation when scrolling to the top of the page
       if (window.scrollY > 0 && window.scrollY > lastScrollY) {
         setNavHidden(true);
-      } else if (window.scrollY + 5 < lastScrollY) {
+      } else if (window.scrollY < lastScrollY) {
         setNavHidden(false);
-      } else if (window.scrollY <= 0) {
-        setNavHidden(true);
       }
-
-      // remember current page location to use in the next move
       setLastScrollY(window.scrollY);
     }
-  };
+  }, [lastScrollY]);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -35,20 +27,18 @@ export default function Nav() {
         window.removeEventListener("scroll", controlNavbar);
       };
     }
-  }, [lastScrollY, navHidden]);
+  }, [lastScrollY, controlNavbar]);
 
-  console.log(navOpen);
+  const navStyle = navOpen
+    ? `${styles.circle} ${styles.navOpen}`
+    : navHidden
+    ? `${styles.circle} ${styles.circleHidden}`
+    : `${styles.circle}`;
 
   return (
     <nav>
       <div
-        className={
-          navOpen
-            ? `${styles.circle} ${styles.navOpen}`
-            : navHidden
-            ? `${styles.circle} ${styles.circleHidden}`
-            : `${styles.circle}`
-        }
+        className={navStyle}
         onClick={() => {
           console.log("click");
           setNavOpen(true);
@@ -64,7 +54,6 @@ export default function Nav() {
           <li>about</li>
           <li>projects</li>
         </ul>
-        <button onClick={() => setNavOpen(false)}>close</button>
       </div>
     </nav>
   );
